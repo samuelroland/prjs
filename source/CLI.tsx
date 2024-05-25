4780422a1d30fbbf6e8d8dac415b4d0c5aea620b
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2024-05-25 16:46:29 +0200

    Add fullscreen support with fullscreen-ink, add support for -v and -h

--- source/CLI.tsx
+++ source/CLI.tsx
@@ -1,29 +1,42 @@
 #!/usr/bin/env node
 import React from 'react';
-import {render} from 'ink';
 import meow from 'meow';
 import App from './App.js';
+import {withFullScreen} from 'fullscreen-ink';
+
+const CLI_NAME = 'PRJS';
+const INTRO = CLI_NAME + ' - Practice Runner for JavaScript';
 
 const cli = meow(
-	`
-	Usage
-	  $ prjs
+	INTRO +
+		'\nRun small JS exos in your terminal with instant feedback loop\n' +
+		'\nUsage' +
+		'\n$ prjs' +
+		'\n\nOptions' +
+		'\n -v, --version: Show version',
+	// Examples
+	//   $ prjs --name=Jane
+	//   Hello, Jane
 
-	Options
-		--name  Your name
-
-	Examples
-	  $ prjs --name=Jane
-	  Hello, Jane
-`,
+	// Supported flags
 	{
 		importMeta: import.meta,
 		flags: {
-			name: {
-				type: 'string',
+			help: {
+				alias: 'h', //just to create an alias -h
 			},
 		},
 	},
 );
 
-render(<App name={cli.flags.name} />);
+// Redefine version display to include CLI name
+cli.showVersion = () => {
+	console.log(CLI_NAME + ': v' + cli.pkg.version);
+};
+
+// Start TUI in fullscreen mode, or output other information and quit
+if (cli.flags.help) {
+	cli.showHelp();
+} else {
+	withFullScreen(<App />).start();
+}
