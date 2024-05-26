a4858e9e89557ddcaf47fdb150b1d4c4f8fc1819
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2024-05-26 02:35:35 +0200

    Create Runner.ts to easily run Vitest

---
+++ source/Runner.ts
@@ -0,0 +1,68 @@
+import { File } from 'vitest';
+import {Vitest, createVitest, } from 'vitest/node';
+import fs from 'fs';
+import util from 'util';
+var log_file = fs.createWriteStream('debug.log', {flags: 'w'});
+// TODO: remove this debug function writing to a log file...
+const log = function (d: any) {
+	log_file.write(util.format(d) + '\n');
+};
+
+export type ExoFile = {
+	filename: string;
+	state: string;
+	path: string;
+}
+
+export class Runner {
+	vt: Vitest | null = null
+	currentFile: string |null;
+	started: boolean
+
+	constructor() {
+		this.currentFile = null
+		this.started = false
+	}
+
+	async startVitest() {
+		if (this.started) return this.getFiles()
+		try {
+			// TODO: make Vitest unable to print things on stdout... Should we use an empty reporter ?
+			this.vt = await createVitest('test',{watch: false, reporters: ["json"], outputFile: "test.json"})
+			await this.vt?.start()
+			log("started !")
+			log(this.vt.state)
+		} catch (error) {
+			log("error !")
+			// console.log(vitest.state.getFiles()[0])
+		}
+		return this.getFiles()
+	}
+
+	async stopVitest() {
+		return this.vt?.close()
+	}
+
+	getFiles(): ExoFile[] {
+		return this.vt?.state.getFiles().map(f => {
+			return {filename: f.name ?? '??', state: f.result?.state ?? 'unknown', path: f.filepath ?? ''}
+		}) ?? []
+	}
+
+	getGivenFile(givenFile: string): File | undefined{
+		return this.vt?.state.getFiles().find(f => f.file?.name === givenFile)
+	}
+
+	setCurrentFile(givenFile: string): boolean {
+		if (!this.getGivenFile(givenFile)) return false
+
+		this.currentFile = givenFile;
+		return true
+	}
+
+	async runCurrentFile() {
+		if (this.currentFile){
+			await this.vt?.rerunFiles([this.currentFile])
+		}
+	}
+}
