b2d1d846454bac415bc81b238b67d2ea079ff52e
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2024-05-26 02:35:58 +0200

    Use Runner in ExosList to show a list of test files, try to fix rendering issues

--- source/ExosList.tsx
+++ source/ExosList.tsx
@@ -1,44 +1,43 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {Box, Text, useInput} from 'ink';
+import {ExoFile, Runner} from './Runner.js';
 
-// Temporary hard coded tests values
-const tests = [
-	{
-		title: 'can create an empty array',
-		expected: [],
-	},
-	{
-		title: 'can create a huge array x elements filled with y',
-		args: [100, 6],
-		expected: new Array(100).fill(6),
-	},
-	{
-		title: 'can create a huge array x elements filled with y (2)',
-		args: [12, 59],
-	},
-	{
-		title:
-			'can push back values and remove at front (push 4 times 10 and pop front 2 times)',
-		args: [[1, 2, 3], 4, 10, 2],
-		expected: [3, 10, 10, 10, 10],
-	},
-	{
-		title: 'can take last element in alphabetic order',
-		args: [['cat', 'dog', 'cow', 'chimp', 'chicken']],
-		expected: 'dog',
-	},
-	{
-		title: 'can remove element between 2 given values',
-		args: [['cat', 'dog', 'cow', 'chimp', 'chicken'], 'dog', 'chimp'],
-		expected: ['cat', 'chicken'],
-	},
-];
+import fs from 'fs';
+import util from 'util';
+import {get} from 'node-emoji';
+var log_file = fs.createWriteStream('debug.log', {flags: 'w'});
+
+const log = function (d: any) {
+	log_file.write(util.format(d) + '\n');
+};
+let lastTime = Date.now() - 1000000;
 
 export function ExosList({}) {
+	const [files, setFiles] = useState<ExoFile[]>([]);
+	const r = new Runner();
+
+	const refreshFiles = async () => {
+		// TODO: make vitest starting just once not at every render...
+		if (lastTime + 50000 > Date.now()) {
+			lastTime = Date.now();
+			return;
+		}
+		log('useeffect');
+		setFiles(await r.startVitest());
+	};
+
+	useEffect(() => {
+		const timer = setInterval(refreshFiles, 500);
+		return () => {
+			clearInterval(timer);
+			r.stopVitest();
+		};
+	});
+
 	const [idx, setIdx] = useState(0); //selected exo index
 	const shortcuts = new Map<string, () => void>();
 	const changeIndex = (newIdx: number) => {
-		if (newIdx >= tests.length || newIdx < 0) return;
+		if (newIdx >= files.length || newIdx < 0) return;
 		setIdx(newIdx);
 	};
 	shortcuts.set('j', () => changeIndex(idx + 1));
@@ -56,13 +55,18 @@ export function ExosList({}) {
 			<Text color="green" bold>
 				Exos list
 			</Text>
+			{/* TODO: add files list */}
+			{/* TODO: show exos in current files */}
 			<Box flexDirection="column" padding={1}>
-				{tests.map((t, i) => (
+				{files.map((f, i) => (
 					<Text
+						key={f.path}
 						backgroundColor={idx == i ? '#0befae' : ''}
 						color={idx == i ? 'black' : ''}
+						wrap="truncate-end"
 					>
-						{i + 1}. {t.title}
+						{i + 1}. {f.filename} {get('check_mark_button')}
+						{get(f.state == 'pass' ? 'check_mark_button' : 'cross_mark')}
 					</Text>
 				))}
 			</Box>
