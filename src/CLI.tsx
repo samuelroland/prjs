b983f218be30098121f337ced6b727c176577369
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2024-08-12 19:11:38 +0200

    Support -v flag for --version, add more colors to --help output

--- src/CLI.tsx
+++ src/CLI.tsx
@@ -4,31 +4,25 @@ import meow from 'meow';
 import App from './App.js';
 import { withFullScreen } from 'fullscreen-ink';
 import Help from './Help.js';
-import { render, Text, Box } from 'ink';
+import { render, Text, Box, Newline } from 'ink';
 import { shortcuts } from './shortcuts.js';
-import { DEFINITION, NAME, SUBDEF } from './util.js';
+import { DEFINITION, NAME, SUBDEF, LOGO_COLORS } from './util.js';
+import Gradient from 'ink-gradient';
 
 const INTRO = NAME + ' - ' + DEFINITION;
 
 const cli = meow(
-	INTRO +
-	'\n' +
-	SUBDEF +
-	'\n' +
-	'\nUsage' +
-	'\n$ prjs' +
-	'\n\nOptions' +
-	'\n -v, --version: Show version' +
-	'\n -h, --help: Show this help' +
-	'\n -d, --debug: Enable debug mode',
-
 	// Supported flags
 	{
 		autoHelp: false, //we want to manage ourself the help (to add -h alias)
+		autoVersion: false,//we want to manage ourself the version (to add -v alias)
 		importMeta: import.meta,
 		flags: {
 			help: {
 				shortFlag: 'h', //just to create an alias -h
+			},
+			version: {
+				shortFlag: 'v', //just to create an alias -v
 			},
 			debug: { // --debug
 				shortFlag: 'd', // create an alias -d
@@ -37,21 +31,33 @@ const cli = meow(
 	},
 );
 
-// Redefine version display to include CLI name
-cli.showVersion = () => {
-	console.log(NAME + ': v' + cli.pkg.version);
-};
+const HELP =
+	'\nUsage' +
+	'\n$ prjs' +
+	'\n\nOptions' +
+	'\n -v, --version: Show version' +
+	'\n -h, --help: Show this help' +
+	'\n -d, --debug: Enable debug mode'
 
 if (cli.flags.help) {
 	// Show the help
 	render(
 		<>
-			<Text>{cli.help}</Text>
-			<Box paddingX={2}>
-				<Help shortcuts={shortcuts} />
+			<Box paddingX={2} paddingY={1} flexDirection='column'>
+				<Gradient colors={LOGO_COLORS}>
+					<Text bold>{INTRO}</Text>
+				</Gradient>
+				<Gradient colors={LOGO_COLORS}>
+					<Text bold>{SUBDEF}</Text>
+				</Gradient>
+				<Text color='magenta'>{HELP}</Text>
+				<Text> </Text>
+				<Help full={true} height={50} shortcuts={shortcuts} />
 			</Box>
 		</>,
 	);
+} else if (cli.flags.version) {
+	console.log(NAME + ' v' + cli.pkg.version);
 } else {
 
 	const debugMode = cli.flags.debug === true ? true : false
